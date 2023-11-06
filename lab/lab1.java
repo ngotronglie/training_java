f872eed04ccff1f9bc92e1dfbc4bed7c8a185a09
Ngo Trong Liem <ngotrongliem2004@gmail.com>
2023-11-06 13:34:24 +0700

    add lession 2

--- lab/lab1.java
+++ lab/lab1.java
@@ -1,2 +1,12 @@
-package lab;public class lab1 {
+package lab;
+
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("lab 1");
+        int hello1 = hello(9);
+        System.out.println(hello1);
+    }
+    public static int hello(int a){
+        return a;
+    }
 }
--- lession2/slide2.java
+++ lession2/slide2.java
@@ -1,2 +1,7 @@
-package lession2;public class slide2 {
+package lession2;
+
+public class slide2 {
+    public static void main(String[] args) {
+        System.out.println("hello lession 2");
+    }
 }
