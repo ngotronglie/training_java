4fad6928d4075eb5d25cceed5b1df99eb3dce79e
Ngo Trong Liem <ngotrongliem2004@gmail.com>
2023-12-11 21:57:40 +0700

    thêm mới

--- lession2/slide2.java
+++ lession2/slide2.java
@@ -3,5 +3,7 @@ package lession2;
 public class slide2 {
     public static void main(String[] args) {
         System.out.println("hello lession 2");
+        System.out.println("Hello mình mới cập nhật lại slide 2");
+
     }
 }
